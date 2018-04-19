568c1bbc38d85f2ebbd61fdc331455b3e02e3254
Riys Hassam <riys@telus.net>
2018-04-18 18:01:53 -0700

    Fix BlockBase.java
    9

--- Master4429 Mod Files/MinecraftForgeGradle/build/sources/main/java/com/hassam/mythocraft/objects/blocks/BlockBase.java
+++ Master4429 Mod Files/MinecraftForgeGradle/build/sources/main/java/com/hassam/mythocraft/objects/blocks/BlockBase.java
@@ -3,6 +3,7 @@ package com.hassam.mythocraft.objects.blocks;
 import com.hassam.mythocraft.Main;
 import com.hassam.mythocraft.util.Reference;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
