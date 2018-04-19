6f98920bf22193fc0d3df4cdc949dc3122b36cab
Riys Hassam <riys@telus.net>
2018-04-18 18:12:56 -0700

    Fix TileEntityHandler.java
    2 errors.

--- Master4429 Mod Files/MinecraftForgeGradle/build/sources/main/java/com/hassam/mythocraft/util/handlers/TileEntityHandler.java
+++ Master4429 Mod Files/MinecraftForgeGradle/build/sources/main/java/com/hassam/mythocraft/util/handlers/TileEntityHandler.java
@@ -1,21 +1,11 @@
 package com.hassam.mythocraft.util.handlers;
 
-
-
-import com.hassam.mythocraft.objects.blocks.machines.sinterer.TileEntitySinteringFurnace;
-
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-
-
 public class TileEntityHandler 
-
 {
-
 	public static void registerTileEntities()
-
 	{
 
 	}
-
 }
