d72f5cc5168a394c0c4b26d7d3db5e918b2e7312
Riys Hassam <riys@telus.net>
2018-04-18 18:15:40 -0700

    Fixed Reference.java
    26 new errors.

--- Master4429 Mod Files/MinecraftForgeGradle/build/sources/main/java/com/hassam/mythocraft/util/Reference.java
+++ Master4429 Mod Files/MinecraftForgeGradle/build/sources/main/java/com/hassam/mythocraft/util/Reference.java
@@ -5,8 +5,8 @@ public class Reference {
 	public static final String NAME = "MythoCraft";
 	public static final String VERSION = "0.0.1ALPHA";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-	public static final String CLIENT_PROXY_CLASS = "com.hassam.mythocraft.proxy.ClientProxy";
-	public static final String COMMON_PROXY_CLASS = "com.hassam.mythocraft.proxy.CommonProxy";
+	public static final String CLIENT = "com.hassam.mythocraft.proxy.ClientProxy";
+	public static final String COMMON = "com.hassam.mythocraft.proxy.CommonProxy";
 
 	public static final int ENTITY_CENTAUR = 120;
 }
